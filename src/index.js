b68051f34d44230aad1700a59a6950341559aa3f
Alex Peterson <alexander.peterson89@gmail.com>
2018-10-15 19:59:26 -0400

    quick router test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App1 from './App1.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import promiseMiddleware from "redux-promise";
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(
+    // <Provider store={createStoreWithMiddleware(reducers)}>
+    // <Provider store={createStoreWithMiddleware(reducers)}>
+
+        <BrowserRouter>
+            <div>
+                {/* <Nav /> */}
+                <Switch>
+                    {/* <Route exact path="/default" component={App} /> */}
+                    <Route exact path="/new" component={App1} />
+                    <Route exact path="/" component={App} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    // </Provider>
+
+
+    //{/* <App />, */}
+
+    ,document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
